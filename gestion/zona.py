1cd6aa23a5b72328b2dc1b246338158e05f508ae
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-03-21 09:29:29 -0500

    Commit

---
+++ gestion/zona.py
@@ -0,0 +1,29 @@
+class Zona:
+
+    def __init__(self, n=None, z=None):
+        self._nombre = n
+        self._zoo = z
+        self._animales = None
+
+    def agregarAnimales(self,a):
+        if self._animales ==None:
+            self._animales = []
+        self._animales.append(a)
+
+    def cantidadAnimales(self):
+        return len(self._animales)
+    
+    def getZoo(self):
+        return self._zoo
+    def setZoo(self,z):
+        self._zoo = z
+    
+    def getNombre(self):
+        return self._nombre
+    def setNombre(self, n):
+        self._nombre = n
+    
+    def getAnimales(self):
+        return Zona._animales
+    
+    
